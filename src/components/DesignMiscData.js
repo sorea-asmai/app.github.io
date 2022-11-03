9432b797143ccf2bb3a2bcf98d619aacf032a7a9
Sorea A <56051252+sorea19@users.noreply.github.com>
2022-11-03 10:03:02 -0700

    Add more design images

--- src/components/DesignMiscData.js
+++ src/components/DesignMiscData.js
@@ -2,6 +2,8 @@ import { FaLine } from "react-icons/fa";
 import mesa from "../assests/CSUS MESA Logo.png"
 import menu from "../assests/MenuPart1.jpg";
 import thirdfws from "../assests/3rdFWS.jpeg";
+import dealership from "../assests/Timor Auto Sales LLC.png"
+import nsm from "../assests/NSM Honor & Scholarship Recipients Presentation.jpg"
 
 const DesignCardData = [
     {
@@ -11,14 +13,30 @@ const DesignCardData = [
         view: "https://www.instagram.com/sacstatemesa/",
         source: ""  
      },
-    
-    {
+
+      {
+        imgsrc: dealership,
+        title: "Dealership",
+        text: "Design business cards, signage for Dealership",
+        view: "https://www.andystokyosushi.com/",
+        source: "https://github.com/femc0d3tt3/LovelaceHacksCollabVersion"  
+      }, 
+
+      {
         imgsrc: thirdfws,
         title: "3rd Future World Symposium",
         text: " Flyer, Powerpoints, etc",
         view: "https://docs.google.com/presentation/d/1LzO4XzvaLpUiTS8OOhfsY66jHA6PwrpP8RGOUZdiejA/edit#slide=id.g7aac8c974e_0_145",
         source: "https://docs.google.com/presentation/d/1LzO4XzvaLpUiTS8OOhfsY66jHA6PwrpP8RGOUZdiejA/edit#slide=id.g7aab8f6d24_0_1293"  
     },
+
+    {
+        imgsrc: nsm,
+        title: "NSM",
+        text: "Presentation",
+        view: "https://www.andystokyosushi.com/",
+        source: "https://github.com/femc0d3tt3/LovelaceHacksCollabVersion"  
+      },
 
     {
         imgsrc: menu,
